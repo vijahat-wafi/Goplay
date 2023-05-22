204e6116fe5659f9887f79e329b14a64afd336f9
vijaht wafi <vijahathunzai@gmail.com>
2023-05-22 06:46:08 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6.3,
+    spaceBetween: 12,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper2 = new Swiper2(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 12,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper3 = new swiper3(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper4 = new Swiper(".mySwiper", {
+    slidesPerView: 8.3,
+    spaceBetween: 12,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
